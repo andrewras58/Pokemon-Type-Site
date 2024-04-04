d832f72ee404d163d717729e174c89116cc8c3ce
andrewras58 <andrewras58@gmail.com>
2024-04-03 21:16:51 -0700

    first commit

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import TypeButtons from "./TypeButtons";
+const Home = () => {
+  return ( 
+    <div className="home">
+      <h1>This is Home</h1>
+      <TypeButtons />
+    </div>
+  );
+}
+ 
+export default Home;
