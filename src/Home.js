03065f35243d0fb6b06b151d9b9a892aae304d78
andrewras58 <andrewras58@gmail.com>
2024-04-04 23:29:51 -0700

    fixed bad merge into main

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,18 @@
 import { useState } from "react";
 import NavBar from "./NavBar";
 import TypeButtons from "./TypeButtons";
-import TypeResult from "./TypeResult";
-import { useState } from "react";
+
 const Home = () => {
+  const [offense, setOffense] = useState(true);
   const [type1, setType1] = useState(null);
   const [type2, setType2] = useState(null);
 
   return ( 
     <div className="home">
-      <h1>This is Home</h1>
+      <NavBar setOffense={setOffense} />
+      <h1>Pokemon - {offense ? "Offensive" : "Defensive"} Type Strengths and Weaknesses</h1>
       <TypeButtons type1={type1} type2={type2} setType1={setType1} setType2={setType2}/>
-      <TypeResult type1={type1} type2={type2}/>
+      
     </div>
   );
 }
