3796be313d82e5162e2a060a27a2907a7204e57e
andrewras58 <andrewras58@gmail.com>
2024-04-07 13:27:38 -0700

    mobile styling plus button logic simplification

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-          </Routes>
-        </div>
-      </div>
-    </Router>
+    <div className="App">
+      <Home />
+    </div>
   );
 }
 
--- src/TypeButtons.js
+++ src/TypeButtons.js
@@ -2,25 +2,10 @@ import { types } from "./type-data";
 
 const TypeButtons = ({type1, type2, offense, setType1, setType2}) => {
   const handleClick = (e) => {
-    if (offense){
-      if (type2){
-        setType2(null);
-      }
-      !type1 || e.target.id !== type1 ? setType1(e.target.id) : setType1(null);  //logic for offense (1 typing)
-    }
-    else{
-      if (!type1){
-        setType1(e.target.id);
-      } else if (e.target.id === type1){
-        setType1(type2);
-        setType2(null);
-      } else if (!type2){
-        setType2(e.target.id);
-      } else if (e.target.id === type2){
-        setType2(null);
-      } else{
-        setType1(e.target.id);
-      }
+    if (!type1){
+      setType1(e.target.id);
+    } else if (!offense && !type2 && type1 !== e.target.id){
+      setType2(e.target.id);
     }
   }
 
@@ -30,11 +15,10 @@ const TypeButtons = ({type1, type2, offense, setType1, setType2}) => {
         <button key={myType} className={`type-button ${myType}`} id={myType} onClick={handleClick}>{myType}</button>
       ))}
       <div className="type-pair">
-        {type1 && <span className={type1}>{type1}</span>}
-        {type2 && <span className={type2}>{type2}</span>}
+        {type1 && <button className={`type-button ${type1} selected`} onClick={ () => {setType1(type2); setType2(null);} }>{type1}<span>X</span></button>}
+        {type2 && <button className={`type-button ${type2} selected`} onClick={ () => setType2(null) }>{type2}<span>X</span></button>}
       </div>
     </div>
   );
 }
-
 export default TypeButtons;
