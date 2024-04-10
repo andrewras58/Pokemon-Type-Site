155cf3e04cc5d883511c2ff0301c08376f0633ff
andrewras58 <andrewras58@gmail.com>
2024-04-10 12:03:27 -0700

    made mobile buttons larger

--- src/Home.js
+++ src/Home.js
@@ -11,8 +11,7 @@ const Home = () => {
   return ( 
     <div className="home">
       <NavBar setOffense={setOffense} setType2={setType2} />
-      <h1>{offense ? "Offensive" : "Defensive"} Type Strengths and Weaknesses</h1>
-      <p>{offense ? "Click the buttons to see offensive type relations" : "Click the buttons to see defensive type relations"}</p>
+      <p>{offense ? "Click the buttons to see offensive type strengths and weaknesses" : "Click the buttons to see defensive type strengths and weaknesses"}</p>
       <TypeButtons type1={type1} type2={type2} offense={offense} setType1={setType1} setType2={setType2}/>
       <TypeResult type1={type1} type2={type2} offense={offense} />
     </div>
